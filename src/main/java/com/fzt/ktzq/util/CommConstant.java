3925ab828f1005bd05c6fc38dd2a947fed8cf93f
黄弋峰 <1648114703@qq.com>
2022-11-27 14:50:16 +0800

    user = 黄弋峰 新增常量类

---
+++ src/main/java/com/fzt/ktzq/util/CommConstant.java
@@ -0,0 +1,26 @@
+package com.fzt.ktzq.util;
+
+/**
+ * 常量类
+ * @author 黄弋峰 2022/11/27
+ */
+public class CommConstant {
+    private CommConstant(){
+
+    }
+    /**错误码**/
+    public static final String ERROR_CODE = "FFFFFF";
+
+    /**错误信息**/
+    public static final String ERROR_MSG = "系统错误";
+
+    /**数据库删除状态对应标志status_cd**/
+    public static final String TATA_DELETE_FLAG = "0";
+
+    /**数据库有效状态对应标志status_cd**/
+    public static final String TATA_ACTIV_FLAG = "1";
+
+    /**成功**/
+    public static final String SUCCESS = "成功";
+
+}
