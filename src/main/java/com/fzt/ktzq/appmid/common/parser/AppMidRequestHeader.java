6da3aa521ab8a71857fbf38268d7918362c78fac
黄弋峰 <1648114703@qq.com>
2022-11-27 15:23:35 +0800

    user = 黄弋峰 新增请求头接口，新增添加字典值服务

---
+++ src/main/java/com/fzt/ktzq/appmid/common/parser/AppMidRequestHeader.java
@@ -0,0 +1,107 @@
+package com.fzt.ktzq.appmid.common.parser;
+
+import com.fzt.ktzq.thoughtwork.xstream.XStreamAlias;
+
+@XStreamAlias("transactionHeader")
+public class AppMidRequestHeader {
+    private String serviceId;
+    private String requestDate;
+    private String requestTime;
+    private String operatorNo;
+    private String txSN;
+    private String channelId;
+    private String mediaType;
+    private Integer pageNo;
+    private Integer pageSize;
+
+    public AppMidRequestHeader(){
+
+    }
+
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(String serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public String getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(String requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public String getRequestTime() {
+        return requestTime;
+    }
+
+    public void setRequestTime(String requestTime) {
+        this.requestTime = requestTime;
+    }
+
+    public String getOperatorNo() {
+        return operatorNo;
+    }
+
+    public void setOperatorNo(String operatorNo) {
+        this.operatorNo = operatorNo;
+    }
+
+    public String getTxSN() {
+        return txSN;
+    }
+
+    public void setTxSN(String txSN) {
+        this.txSN = txSN;
+    }
+
+    public String getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(String channelId) {
+        this.channelId = channelId;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public Integer getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(Integer pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    @Override
+    public String toString() {
+        return "AppMidRequestHeader{" +
+                "serviceId='" + serviceId + '\'' +
+                ", requestDate='" + requestDate + '\'' +
+                ", requestTime='" + requestTime + '\'' +
+                ", operatorNo='" + operatorNo + '\'' +
+                ", txSN='" + txSN + '\'' +
+                ", channelId='" + channelId + '\'' +
+                ", mediaType='" + mediaType + '\'' +
+                ", pageNo=" + pageNo +
+                ", pageSize=" + pageSize +
+                '}';
+    }
+}
