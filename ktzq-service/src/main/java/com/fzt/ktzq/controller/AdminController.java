c5075375fe6a05d223f7562b3cb9bd3ea7daee2c
黄弋峰 <1648114703@qq.com>
2023-01-03 15:59:41 +0800

    user = 黄弋峰 添加依赖项

--- ktzq-service/src/main/java/com/fzt/ktzq/controller/AdminController.java
+++ ktzq-service/src/main/java/com/fzt/ktzq/controller/AdminController.java
@@ -13,10 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author 黄弋峰 2023/1/3
  */
+@RestController
 public class AdminController {
     private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
 
