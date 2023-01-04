54d50f7f2bf9f9318f4a971056965611e4b77b95
黄弋峰 <1648114703@qq.com>
2023-01-04 15:51:08 +0800

    user = 黄弋峰 修改bug

--- ktzq-dao/src/main/java/com/fzt/ktzq/dao/Admin.java
+++ ktzq-dao/src/main/java/com/fzt/ktzq/dao/Admin.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "u_admin")
 public class Admin implements Serializable {
+    @Id
+    private Integer id;
     private String userName;
     private Integer collegeId;
     private String collegeName;
