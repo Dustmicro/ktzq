04a40e1e06e57bc9bb91cf879febb58a6666cc5b
HYF <1648114703@qq.com>
2023-01-17 14:58:50 +0800

    user = 黄弋峰 初步构思考勤机制

---
+++ ktzq-dao/src/main/java/com/fzt/ktzq/dao/RecordDay.java
@@ -0,0 +1,37 @@
+package com.fzt.ktzq.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 每日考勤实体类
+ * @author 黄弋峰 2023/1/17
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "record_day")
+public class RecordDay implements Serializable {
+
+    @Id
+    private Integer recordId;
+
+    private Integer memberId;
+
+    private String state;
+
+    private Date createTime;
+
+    private String mark;
+
+    private String statusCd;
+
+    private static final long serialVersionUID = 1L;
+
+}
