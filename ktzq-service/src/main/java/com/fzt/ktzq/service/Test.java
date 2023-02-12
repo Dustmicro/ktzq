f560ffe51290df6b3a2ff131e40f07935563a872
HYF <1648114703@qq.com>
2023-02-12 11:20:08 +0800

    user = 黄弋峰 更新Test测试类

--- ktzq-service/src/main/java/com/fzt/ktzq/service/Test.java
+++ ktzq-service/src/main/java/com/fzt/ktzq/service/Test.java
@@ -17,20 +17,46 @@ public class Test {
 //        } else {
 //            System.out.println("不可以");
 //        }
-        String str = "精品大额存单死锅唉";
-        System.out.println("原字符串为：" + str);
-        System.out.println("后四位是：" + str.substring(0, str.length()-4));
-        if (str.contains("大额存单")){
-            System.out.println("匹配成功");
-            return;
-        } else {
-            System.out.println("匹配不通过");
-        }
+        /**
+         * 验证字符串是否包含指定字段
+         */
+//        String str = "精品大额存单死锅唉";
+//        System.out.println("原字符串为：" + str);
+//        System.out.println("后四位是：" + str.substring(0, str.length()-4));
+//        if (str.contains("大额存单")){
+//            System.out.println("匹配成功");
+//            return;
+//        } else {
+//            System.out.println("匹配不通过");
+//        }
 //        String str = "A";
 //        if (str != null && ("A".equals(str) || "B".equals(str))){
 //            System.out.println("满足");
 //        }
 //        Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");//"DES/CBC/PKCS5Padding"是一个算法名称
 
+        /**
+         * for循环遍历测试
+         */
+//        String[] str = new String[3];
+//        str[0] = "1";
+//        str[1] = "6";
+//        str[2] = "3";
+//        String a = "";
+//        for (int i = 0; i < str.length; i++){
+//            if ("2" == str[i]){
+////                System.out.println("可以匹配到");
+//                a = "AAAAAA";
+//                break;
+//            } else {
+////                System.out.println("不行");
+//                a = "FFFFFF";
+//            }
+//        }
+//        if ("FFFFFF" != a){
+//            System.out.println("最终结果是" + a);
+//        } else {
+//            System.out.println("huhuhu" + a);
+//        }
     }
 }
